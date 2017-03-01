9291bb33926fdb32a3a6912d81301809fbf66e0e
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-01 13:50:18 -0600

    function logs out argument as a cheer

--- cheer.js
+++ cheer.js
@@ -5,11 +5,22 @@ const cheer = (name) => {
   let cheerCaps = name.toUpperCase();
   //take the now uppercase string and turn it to an array
   let cheerArray = Array.from(cheerCaps);
-
 // logged to the console/terminal
-// with the argument passed to it
+  for(var i=0; i < cheerArray.length; i++) {
 // account for a/an
+  if(cheerArray[i] === "A" | "E" | "I" | "O" | "U") {
+    console.log(`Give me an ${cheerArray[i]}`);
+    } else {
+      console.log(`Give me a ${cheerArray[i]}`);
+    }
+    console.log(`What's that spell?`);
+    } // end of for loop
+    console.log(`${cheerCaps}!`);
+  }
+
+
+// with the argument passed to it
 // delay the logs by 1 second.
-}
+
 
 cheer("Bob");
